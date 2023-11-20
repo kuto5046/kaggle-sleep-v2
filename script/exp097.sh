dbf44e1e25bf722e755f28f115b6a1519eeffda7
Kyohei Uto <h27.kubb@gmail.com>
2023-11-21 01:40:45 +0900

    Add experiment configuration files and training script for exp096 and exp097

---
+++ script/exp097.sh
@@ -0,0 +1,8 @@
+rye run python run/train.py +experiment=exp097 epoch=20 model.encoder_name=efficientnet-b0
+rye run python run/train.py +experiment=exp097 epoch=20 model.encoder_name=efficientnet-b1
+rye run python run/train.py +experiment=exp097 epoch=20 model.encoder_name=efficientnet-b2
+rye run python run/train.py +experiment=exp097 epoch=20 model.encoder_name=efficientnet-b3
+rye run python run/train.py +experiment=exp097 epoch=20 model.encoder_name=efficientnet-b4
+rye run python run/train.py +experiment=exp097 epoch=20 model.encoder_name=resnext50_32x4d
+rye run python run/train.py +experiment=exp097 epoch=20 model.encoder_name=timm-resnest14d
+rye run python run/train.py +experiment=exp097 epoch=20 model.encoder_name=timm-resnest50d
