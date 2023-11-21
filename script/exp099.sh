3b5f4326fd905a78e33ebbdf5a72f2f52e3f0200
Kyohei Uto <h27.kubb@gmail.com>
2023-11-21 19:15:25 +0900

    Add experiment configuration and training script for exp099

---
+++ script/exp099.sh
@@ -0,0 +1,3 @@
+rye run python run/train.py +experiment=exp099 epoch=20 model.encoder_name=mit_b0
+rye run python run/train.py +experiment=exp099 epoch=20 model.encoder_name=mit_b1
+rye run python run/train.py +experiment=exp099 epoch=20 model.encoder_name=mit_b2
