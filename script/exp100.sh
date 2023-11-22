e9de95aea7f2ed4ee257fd98f0e1389f56c1f0cf
Kyohei Uto <h27.kubb@gmail.com>
2023-11-23 00:11:24 +0900

    Add experiment configurations for exp100 and exp101

---
+++ script/exp100.sh
@@ -0,0 +1,3 @@
+rye run python run/train.py hydra.mode=MULTIRUN +experiment=exp100 split=fold_0,fold_1,fold_2,fold_3,fold_4
+rye run python run/train.py hydra.mode=MULTIRUN +experiment=exp101 split=fold_0,fold_1,fold_2,fold_3,fold_4
+
