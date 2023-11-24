771d73397ff7762490345fe19e2a5c011d2d8d10
Kyohei Uto <h27.kubb@gmail.com>
2023-11-24 20:46:47 +0900

    Add experiment configuration and script for exp107

---
+++ script/exp107.sh
@@ -0,0 +1,3 @@
+rye run python run/train.py hydra.mode=MULTIRUN +experiment=exp107 imbalanced_loss_weight=2
+rye run python run/train.py hydra.mode=MULTIRUN +experiment=exp107 imbalanced_loss_weight=3
+rye run python run/train.py hydra.mode=MULTIRUN +experiment=exp107 imbalanced_loss_weight=4
