0b8a0f159d7c034cba02f7a9d0ab86e6ba910b9d
Kyohei Uto <h27.kubb@gmail.com>
2023-11-26 17:07:34 +0900

    Add new experiments and update decoder configurations

--- script/exp111.sh
+++ script/exp111.sh
@@ -1,4 +1,9 @@
-rye run python run/train.py +experiment=exp111 decoder=TransformerDecoder decoder.num_layers=2 decoder.dropout=0.5
-rye run python run/train.py +experiment=exp111 decoder=MLPDecoder
-rye run python run/train.py +experiment=exp111 decoder=LSTMDecoder
-rye run python run/train.py +experiment=exp111 decoder=CNN1DLSTMDecoder
+# rye run python run/train.py +experiment=exp111 decoder=TransformerDecoder decoder.num_layers=2 decoder.dropout=0.5
+# rye run python run/train.py +experiment=exp111 decoder=MLPDecoder
+# rye run python run/train.py +experiment=exp111 decoder=LSTMDecoder
+# rye run python run/train.py +experiment=exp111 decoder=CNN1DLSTMDecoder
+# rye run python run/train.py +experiment=exp111 decoder=LSTMDecoder decoder.dropout=0.7
+# rye run python run/train.py +experiment=exp111 decoder=LSTMDecoder decoder.num_layers=1
+
+rye run python run/train.py hydra.mode=MULTIRUN +experiment=exp111 decoder=LSTMDecoder split=fold_0,fold_1,fold_2,fold_3,fold_4 model.encoder_name=resnet18
+rye run python run/train.py hydra.mode=MULTIRUN +experiment=exp112 decoder=LSTMDecoder split=fold_0,fold_1,fold_2,fold_3,fold_4 model.encoder_name=timm-efficientnet-b2
