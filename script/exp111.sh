3f4ceb10f7b34013813fd3c592fae0a9997f64d4
Kyohei Uto <h27.kubb@gmail.com>
2023-11-26 15:36:31 +0900

    Update decoder names in exp111.sh script

--- script/exp111.sh
+++ script/exp111.sh
@@ -1,4 +1,4 @@
-rye run python run/train.py +experiment=exp111 decoder.name=TransformerDecoder decoder.num_layers=2 decoder.dropout=0.5
-rye run python run/train.py +experiment=exp111 decoder.name=MLPDecoder
-rye run python run/train.py +experiment=exp111 decoder.name=LSTMDecoder
-rye run python run/train.py +experiment=exp111 decoder.name=CNN1DLSTMDecoder
+rye run python run/train.py +experiment=exp111 decoder=TransformerDecoder decoder.num_layers=2 decoder.dropout=0.5
+rye run python run/train.py +experiment=exp111 decoder=MLPDecoder
+rye run python run/train.py +experiment=exp111 decoder=LSTMDecoder
+rye run python run/train.py +experiment=exp111 decoder=CNN1DLSTMDecoder
