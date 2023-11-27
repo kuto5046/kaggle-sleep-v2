518ee07c4040f1bcd9e194bc8700b7474f490756
Kyohei Uto <h27.kubb@gmail.com>
2023-11-28 00:23:22 +0900

    Add experiment configuration and script for exp117

---
+++ script/exp117.sh
@@ -0,0 +1,6 @@
+rye run python run/train.py +experiment=exp117 model.encoder_name=resnet18
+rye run python run/train.py +experiment=exp117 model.encoder_name=resnet34
+rye run python run/train.py +experiment=exp117 model.encoder_name=resnet50
+rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2
+rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2 model.encoder_weights=advprop
+rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2 model.encoder_weights=noisy-student
