b6fd0c280a6306c51b466f53ae7a9cfd352894ed
Kyohei Uto <h27.kubb@gmail.com>
2023-11-29 00:16:05 +0900

    Update experiment configurations

---
+++ script/exp121.sh
@@ -0,0 +1,4 @@
+rye run python run/train.py +experiment=exp121
+rye run python run/train.py +experiment=exp122
+rye run python run/train.py +experiment=exp123
+rye run python run/train.py +experiment=exp124
