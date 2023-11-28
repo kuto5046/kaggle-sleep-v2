7a309ff5641694b842b73fff7bd581277aea812b
Kyohei Uto <h27.kubb@gmail.com>
2023-11-28 09:06:13 +0900

    add

--- script/exp117.sh
+++ script/exp117.sh
@@ -1,6 +1,7 @@
-rye run python run/train.py +experiment=exp117 model.encoder_name=resnet18
-rye run python run/train.py +experiment=exp117 model.encoder_name=resnet34
-rye run python run/train.py +experiment=exp117 model.encoder_name=resnet50
-rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2
-rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2 model.encoder_weights=advprop
-rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2 model.encoder_weights=noisy-student
+# rye run python run/train.py +experiment=exp117 model.encoder_name=resnet18
+# rye run python run/train.py +experiment=exp117 model.encoder_name=resnet34
+# rye run python run/train.py +experiment=exp117 model.encoder_name=resnet50
+# rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2
+# rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2 model.encoder_weights=advprop
+# rye run python run/train.py +experiment=exp117 model.encoder_name=timm-efficientnet-b2 model.encoder_weights=noisy-student
+rye run python run/train.py +experiment=exp117 scheduler.warmup_step_rate=0.05
