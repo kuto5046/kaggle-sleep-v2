1268357aa338b14afcb5599d3e78c3b774bba5dd
Kyohei Uto <h27.kubb@gmail.com>
2023-11-28 22:48:35 +0900

    Add experiment configuration file and update training script

--- script/exp119.sh
+++ script/exp119.sh
@@ -1,2 +1,4 @@
-rye run python run/train.py +experiment=exp119 decoder.hidden_size=128
-rye run python run/train.py +experiment=exp119 decoder.hidden_size=64
+# rye run python run/train.py +experiment=exp119 decoder.hidden_size=128
+# rye run python run/train.py +experiment=exp119 decoder.hidden_size=64
+# rye run python run/train.py +experiment=exp119 decoder.hidden_size=128 decoder.num_layers=3
+rye run python run/train.py +experiment=exp119 decoder.hidden_size=128 decoder.num_layers=3 decoder.dropout=0.5
